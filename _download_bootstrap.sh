221926db27f1adfd3b43e693ae2d78828befc198
Daniel Vizzini <dvizzini@gmail.com>
2013-01-04 19:21:54 -0500

    modified download script

--- _download_bootstrap.sh
+++ _download_bootstrap.sh
@@ -2,10 +2,10 @@
 
 #
 # if this gets too hectic, see http://brizzled.clapper.org/blog/2012/03/05/using-twitter-bootstrap-with-jekyll/
-#
+# 
 
 rm -rf master bootstrap
-wget https://github.com/stuyopensource/bootstrap/zipball/master
+wget https://github.com/stuyopensouce/bootstrap/zipball/master
 unzip -q master
-mv stuyopensource-bootstrap-*/bootstrap .
-rm -rf stuyopensource-bootstrap-*
+mv stuyopensouce-bootstrap-*/bootstrap .
+rm -rf stuyopensouce-bootstrap-*
